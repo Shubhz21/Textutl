d37ed2836c5fcd76d720f52f8e1d063cb51b09c1
shubham funde <shubhamfunde2222@gmail.com>
2023-02-28 21:10:35 +0530

    newdt

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -58,7 +58,7 @@ export default function TextForm(props) {
 <button className="btn btn-danger mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
 <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear</button>
 <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>Clear extra spaces</button>
-<button className="btn btn-primary mx-1" onClick={handleCopy}>Copy</button>
+{/* <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy</button> */}
 
     </div>
     <div className="container my-3">
